431769e4cb482e35e1753a5b4137b180e465c986
Pradimarhm <pradanaivan574@gmail.com>
2024-09-10 10:20:56 +0700

    Create tugas12.php

---
+++ Acara 12/tugas12.php
@@ -0,0 +1,38 @@
+<?php
+// Abstract class Person
+abstract class Person {
+    abstract public function greet();
+}
+
+// Class English
+class English extends Person {
+    public function greet() {
+        echo "Hello\n";
+    }
+}
+
+// Class German
+class German extends Person {
+    public function greet() {
+        echo "Hallo\n";
+    }
+}
+
+// Class French
+class French extends Person {
+    public function greet() {
+        echo "Bonjour\n";
+    }
+}
+
+// Contoh penggunaan polymorphism
+$people = [
+    new English(),
+    new German(),
+    new French()
+];
+
+foreach ($people as $person) {
+    $person->greet();
+}
+?>
