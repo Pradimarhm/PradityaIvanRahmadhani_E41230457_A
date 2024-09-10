88824bb0fc741cbc26ace3895d0aadbd5c29e078
Pradimarhm <pradanaivan574@gmail.com>
2024-09-10 10:22:32 +0700

    Create installed.php

---
+++ Acara 13/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'praditya/acara13',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '78836e4f7609f19487462395517d50b41b505b40',
+        'type' => 'latihan',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'praditya/acara13' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '78836e4f7609f19487462395517d50b41b505b40',
+            'type' => 'latihan',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
