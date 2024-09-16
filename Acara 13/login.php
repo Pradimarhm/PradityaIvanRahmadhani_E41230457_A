cf0d892c253b2a0282cb569e62271459dd58fb97
Pradimarhm <pradanaivan574@gmail.com>
2024-09-17 01:04:55 +0700

    Update login.php

--- Acara 13/login.php
+++ Acara 13/login.php
@@ -22,6 +22,7 @@
 
             if ($num != 0) {
                 if ($userVal==$email && $passVal==$pass) {
+                    $_SESSION['user_email']=$userVal;
                     header('location: home.php');
                 }else{
                     $error = 'user atau password salah!!';
@@ -46,11 +47,32 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
-<body>
-    <form action="login.php" method="POST">
-        <p>email $nbsp;$nbsp;$nbsp;$nbsp;$nbsp;$nbsp;: <input type="text" name="txt_email"></p>
-        <p>password : <input type="password" name="txt_pass"></p>
-        <button type="submit" name="submit">Sign In</button>
-    </form>
+<body class="bg-primary">
+    <div class="container mt-5" >
+        <div class="row justify-content-center" style="margin-top: 10rem;">
+                <div class="col-md-4 shadow-lg p-3 mb-5 rounded" style="background-color: white;" >
+                    <div class="d-block p-2" >
+                        <div class="card-header text-center">
+                            <h3 >Login</h3>
+                        </div>
+                        <div>
+                            <form action="login.php" method="POST">
+                                <div class="mb-4">
+                                    <label for="email" class="form-label" >Email</label>
+                                    <input type="email" class="form-control" placeholder="Masukkan Email" name="txt_email"  required>
+                                </div>
+                                <div class="mb-4">
+                                    <label for="password" class="form-label" >Password</label>
+                                    <input type="password" class="form-control" placeholder="Masukkan Email" name="txt_pass"  required>
+                                </div>
+                                <div class="d-grid" style="margin-top: 6rem;">
+                                    <button type="submit" class="btn btn-primary"  name="submit">Sign In</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </div>
 </body>
 </html>
