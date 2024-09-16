d69b46afc3d4c7fee78fd0ef8864a9ef3a3eef1f
Pradimarhm <pradanaivan574@gmail.com>
2024-09-17 01:04:48 +0700

    Update edit.php

--- Acara 13/edit.php
+++ Acara 13/edit.php
@@ -1,27 +1,29 @@
 <?php
     require("koneksi.php");
-    if (isset($_POST['update'])) {
-        $userId = $_POST['txt_id'];
-        $userMail = $_POST['txt_email'];
-        $userPass = $_POST['txt_pass'];
-        $userName = $_POST['txt_nama'];
-
-        $query = "UPDATE user_detail SET user_password='$userPass', user_fullname='$userName', WHERE id='$userPass'";
-        echo $query;
-        $result = mysqli_query($koneksi,$query);
-        header('location: login.php');
-
-    }
     $id = $_GET['id'];
     $query = "SELECT * FROM user_detail WHERE id='$id'";
     $result = mysqli_query($koneksi, $query) or die(mysqli_error($koneksi));
 
     while ($row = mysqli_fetch_array($result)) {
-        $id = $row['id'];
+        $idUser = $row['id'];
         $userMail = $row['user_email'];
         $userPass = $row['user_password'];
         $userName = $row['user_fullname'];
     }
+
+    if (isset($_POST['update'])) {
+        $userId = $idUser;
+        $userMail = $_POST['txt_email'];
+        $userPass = $_POST['txt_pass'];
+        $userName = $_POST['txt_nama'];
+
+        $query = "UPDATE user_detail SET user_password='$userPass', user_fullname='$userName' WHERE id='$userId'";
+        echo $query;
+        $result = mysqli_query($koneksi,$query);
+        header('location: login.php');
+
+    }
+    
 ?>
 
 <!-- <!DOCTYPE html>
@@ -55,44 +57,47 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
-<body>
-    <div class="container mt-5">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card">
-                    <div class="card-header text-center">
-                        <h3>Edit Akun</h3>
-                    </div>
-                    <div class="card-body">
-                        <form action="edit.php" method="POST">
-                            <input type="hidden" name="id" value="<?php echo $id;?>">
+<body class="bg-primary">
+    <div style="margin-top: 10rem;">
+        <div class="container mt-5" >
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-header text-center">
+                            <h3>Edit Akun</h3>
+                        </div>
+                        <div class="card-body">
+                            <form action="edit.php" method="post">
+                                <input type="hidden" name="id" value="<?php echo $id;?>">
 
-                            <div class="mb-3">
-                                <label for="email" class="form-label">Email</label>
-                                <input type="email" class="form-control" id="email" name="txt_email" value="<?php echo $userMail?>" readonly>
-                            </div>
+                                <div class="mb-3">
+                                    <label for="email" class="form-label">Email</label>
+                                    <input type="email" class="form-control" id="email" name="txt_email" value="<?php echo $userMail?>" readonly>
+                                </div>
 
-                            <div class="mb-3">
-                                <label for="password" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="password" name="txt_pass" value="<?php echo $userPass;?>">
-                            </div>
+                                <div class="mb-3">
+                                    <label for="password" class="form-label">Password</label>
+                                    <input type="password" class="form-control" id="password" name="txt_pass" value="<?php echo $userPass;?>">
+                                </div>
 
-                            <div class="mb-3">
-                                <label for="name" class="form-label">Nama</label>
-                                <input type="text" class="form-control" id="name" name="txt_nama" value="<?php echo $userName?>">
-                            </div>
+                                <div class="mb-3">
+                                    <label for="name" class="form-label">Nama</label>
+                                    <input type="text" class="form-control" id="name" name="txt_nama" value="<?php echo $userName?>">
+                                </div>
 
-                            <div class="d-grid">
-                                <button type="submit" name="update" class="btn btn-primary">Update</button>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="card-footer text-center">
-                        <a href="home.php" class="btn btn-link">Kembali</a>
+                                <div class="d-grid">
+                                    <button type="submit" name="update" class="btn btn-primary">Update</button>
+                                </div>
+                            </form>
+                        </div>
+                        <div class="card-footer text-center">
+                            <a href="home.php" class="btn btn-link">Kembali</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    
 </body>
 </html>
