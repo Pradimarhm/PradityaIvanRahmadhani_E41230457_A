2fe61a0aeb35d5e981a7edb039b3f21c64a69021
Pradimarhm <pradanaivan574@gmail.com>
2024-10-03 20:36:35 +0700

    Auto commit changes

--- acara17/proses_act.php
+++ acara17/proses_act.php
@@ -4,7 +4,8 @@ $limit = 10 * 1024 * 1024;
 $ekstensi = array('png', 'jpg', 'jpeg', 'gif');
 $jumlahFile = count($_FILES['foto']['name']);
 
-for ($x = 0; $x < $jumlahFile; $x++) {
+
+for ($x = 0; $x < $jumlahFile; $x++ ) {
     $namafile = $_FILES['foto']['name'][$x];
     $tmp = $_FILES['foto']['tmp_name'][$x];
     $tipe_file = pathinfo($namafile, PATHINFO_EXTENSION);
@@ -12,21 +13,21 @@ for ($x = 0; $x < $jumlahFile; $x++) {
 
     if ($ukuran > $limit) {
         header("location:index.php?alert=gagal_ukuran");
-        exit();
+        // exit();
     } 
     
     else {
         if (!in_array(strtolower($tipe_file), $ekstensi)) {
             header("location:index.php?alert=gagal_ektensi");
-            exit();
+            // exit();
         } 
         
         else {
             move_uploaded_file($tmp, 'file/' . date('d-m-Y') . '-' . $namafile);
-            $x = date('d-m-Y') . '-' . $namafile;
-            mysqli_query($koneksi, "INSERT INTO gambar VALUES(NULL, '$x')");
+            $namaGambarDatabase = date('d-m-Y') . '-' . $namafile;
+            mysqli_query($koneksi, "INSERT INTO gambar VALUES(NULL, '$namaGambarDatabase')");
             header("location:index.php?alert=simpan");
-            exit();
+            // exit();
         } 
     }
 }
